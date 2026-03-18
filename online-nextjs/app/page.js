2cd6bea25318175a11566c36663193495d4f7e2e
Santam Sarkar <santamsarkar@gmail.com>
2026-03-18 17:08:56 +0530

    --

--- online-nextjs/app/page.js
+++ online-nextjs/app/page.js
@@ -13,6 +13,7 @@ export default function HomePage() {
       <div dangerouslySetInnerHTML={{ __html: bodyHtml }} />
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js" strategy="afterInteractive" />
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" strategy="afterInteractive" />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js" strategy="afterInteractive" />
       <Script src="/youme.js" strategy="afterInteractive" />
     </>
   );
